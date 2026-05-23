2e4e4f50f7e669ea3563f1b6da1f3d79dcb6550b
wonmin3180 <wonmin3180@gmail.com>
2026-05-23 12:55:01 +0900

    initial commit

---
+++ src/main/java/org/example/toy_4_be/Toy4BeApplication.java
@@ -0,0 +1,13 @@
+package org.example.toy_4_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy4BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toy4BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/toy_4_be/Toy4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.toy_4_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy4BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
